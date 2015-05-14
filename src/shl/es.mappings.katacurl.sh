100ccad1991f8ee742afac8256e438a3fb582965
Ard0ise <ardoise.gisement@gmail.com>
2015-05-14 18:00:00 +0200

    add index_src and index_dest

--- src/shl/es.mappings.katacurl.sh
+++ src/shl/es.mappings.katacurl.sh
@@ -32,6 +32,9 @@ ES_URL_FROM=${ES_URL_FROM:-$ES_URL_DEFAULT}; echo "ENV ES_URL_FROM=${ES_URL_FROM
 ES_URL_TO=${ES_URL_TO:-$ES_URL_DEFAULT}; echo "ENV ES_URL_TO=${ES_URL_TO}";
 ES_INDEX_DEFAULT=test; echo "ENV ES_INDEX_DEFAULT=${ES_INDEX_DEFAULT}";
 ES_INDEX=${ES_INDEX:-$ES_INDEX_DEFAULT}; echo "ENV ES_INDEX=${ES_INDEX}";
+ES_INDEX_SRC=${ES_INDEX_SRC:-$ES_INDEX}; echo "ENV ES_INDEX_SRC=${ES_INDEX_SRC}";
+ES_INDEX_DEST=${ES_INDEX_DEST:-$ES_INDEX}; echo "ENV ES_INDEX_DEST=${ES_INDEX_DEST}";
+
 
 [[ "${ES_URL_FROM}" == "${ES_URL_TO}" ]] && (
   cmd="WARN : ES_URL_FROM='${ES_URL_FROM}' == ES_URL_TO='${ES_URL_TO}' are the same."
@@ -76,7 +79,9 @@ function require_jq {
 }
 
 function es-copy-mapping-curl {
-  ARG1=${1:-$ES_INDEX};    #index
+  ARG1=${1:-$ES_INDEX_SRC};    #index source
+  ARG2=${2:-$ES_INDEX_DEST};   #index destination
+
   echo -e "\n#COPY SETTINGS,MAPPINGS,ALIASES,WARNERS $ARG1 from url_source to url_dest";
   
   #REQUIRES
@@ -90,9 +95,8 @@ function es-copy-mapping-curl {
   )
   # CURL METHODE
   # for-each [ index ]
-  #   es-export ((_settings,_mappings), _aliases, _warmers) $index ${ES_URL_FROM}_url
-  #   es-import ((_settings,_mappings), _aliases, _warmers) $index ${ES_URL_TO}_url
-  # !!! _settings,_mappings couple qui ne peut pas etre separes pour une seule passe
+  #   es-export ((_settings,_mappings), _aliases, _warmers) $ARG1 ${ES_URL_FROM}_url
+  #   es-import ((_settings,_mappings), _aliases, _warmers) $ARG2 ${ES_URL_TO}_url
   cmd="curl -XGET '${ES_URL_FROM}/${ARG1}/_settings,_mapping,_aliases,_warmers' |jq '.${ARG1}' >/tmp/${ARG1}.smaw.json"; 
   echo "RUN ${cmd}";
   eval ${cmd}; 
@@ -100,18 +104,21 @@ function es-copy-mapping-curl {
   cmd="ls -ail /tmp/${ARG1}.smaw.json";
   echo "RUN ${cmd}";
 
-  cmd="curl -XPUT '${ES_URL_TO}/${ARG1}' -d @/tmp/${ARG1}.smaw.json && echo"; 
+  cmd="curl -XPUT '${ES_URL_TO}/${ARG2}' -d @/tmp/${ARG1}.smaw.json && echo"; 
   echo "RUN ${cmd}"; 
   eval ${cmd};
 }
 
 function es-process-data {
-  ARG1=${1:-$ES_INDEX};    #index
+  ARG1=${1:-$ES_INDEX_SRC};    #index source
+  ARG2=${2:-$ES_INDEX_DEST};   #index destination
+
   echo -e "\n#PROCESS\n";
 
   [ -z "$ARG1" ] && exit 1
+  [ -z "$ARG2" ] && exit 1
   
-  es-copy-mapping-curl ${ARG1};   #copy MAPPING from ${ES_URL_FROM} to ${ES_URL_TO}
+  es-copy-mapping-curl ${ARG1} ${ARG2};   #copy MAPPING from ${ES_URL_FROM}/${ARG1} to ${ES_URL_TO}/${ARG2}
 }
 
 # ==== MAIN ===
